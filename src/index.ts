8ae1e28241ed98ea8a7e4a956804f5bdfd604a50
Sergeew Kirill <kirill.sergeev@progforce.com>
2022-08-19 17:39:53 +0300

    feat: created exrpress project with server

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import data from '../db.json'
+
+const app = express()
+const PORT = 8080
+
+function startServer() {
+  app.listen(PORT, () => {
+    console.log('Server started on PORT: ', PORT)
+  })
+  app.get('/', (req, res) => {
+    res.send(data)
+  })
+}
+
+startServer()
