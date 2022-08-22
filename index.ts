6f5ebad3e6f8db51d6f2a879d76aab49ec74f641
Sergeew Kirill <kirill.sergeev@progforce.com>
2022-08-22 10:53:29 +0300

    feat: added config for vercel and little fix index

--- index.ts
+++ index.ts
@@ -5,12 +5,22 @@ const app = express()
 const PORT = 8080
 
 function startServer() {
-  app.listen(PORT, () => {
-    console.log('Server started on PORT: ', PORT)
-  })
-  app.get('/', (req, res) => {
-    res.send(data)
-  })
+  try {
+    app.get('/', (req, res) => {
+      res.send('<h1>Server started on vercel</h1>')
+    })
+    app.get('/products', (req, res) => {
+      res.send(data)
+    })
+
+    app.listen(PORT, () => {
+      console.log('Server started on PORT: ', PORT)
+    })
+  } catch (error) {
+    alert(`Error ${error}`)
+  }
 }
 
 startServer()
+
+module.exports = app
