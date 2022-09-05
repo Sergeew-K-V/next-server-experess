0ea341a316361202ea9047f18eda06fbb4828d04
Sergeew Kirill <kirill.sergeev@progforce.com>
2022-09-05 14:26:02 +0300

    refactor: changed path

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ function startServer() {
     app.get('/', (req, res) => {
       res.send('<h1>Server started on vercel</h1>')
     })
-    app.get('/products', (req, res) => {
+    app.get('/menu', (req, res) => {
       res.send(data)
     })
 
