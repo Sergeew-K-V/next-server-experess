46d5a0ba0947533c6a9ab4b309a9c082a523e3c7
Sergeew Kirill <kirill.sergeev@progforce.com>
2022-09-26 18:00:09 +0300

    refactor: built architech for labs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express'
-import data from './db.json'
+import data from '../db.json'
 import dotenv from 'dotenv'
 import path from 'path'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
 dotenv.config()
 const app = express()
@@ -10,16 +11,30 @@ const PORT = process.env.PORT || 8081
 
 app.use(cors())
 
+const ConnectionToDb = async () => {
+  try {
+    if (process.env.DATABASE && process.env.DB_NAME !== undefined) {
+      const connection = await mongoose.connect(process.env.DATABASE + '/' + process.env.DB_NAME)
+      return console.log(`CONNECTION TO ${process.env.DB_NAME} DATABASE - COMPLETED`)
+    }
+  } catch (error) {
+    return console.log('NOT CONNECTION TO DATABASE')
+  }
+}
+
 function startServer() {
   try {
+    app.use('/static', express.static(path.join(__dirname, 'public')))
+    ConnectionToDb()
+    const DB = mongoose.connection
+
     app.get('/', (req, res) => {
       res.send('<h1>Server started on vercel</h1>')
     })
+
     app.get('/menu', (req, res) => {
       res.send(data)
     })
-
-    app.use('/static', express.static(path.join(__dirname, 'public')))
 
     app.listen(PORT, () => {
       console.log('Server started on PORT: ', PORT)
